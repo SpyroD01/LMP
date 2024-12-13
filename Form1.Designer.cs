bf2c6b383c87b764f8fe6aff5096285ea960f055
Neftali <natalionataly@gmail.com>
2024-12-13 09:56:26 -0700

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace LMP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.equiposToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.equiposToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.jugadoresToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.estadiosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.estadísticasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.partidosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.estadísticasDeEquiposToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.bateadoresToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pitchersToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clasificaciónToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aficionadosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.horariosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.plataformasDeDistribuciónToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.menuStrip1.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.equiposToolStripMenuItem,
+            this.estadísticasToolStripMenuItem,
+            this.aficionadosToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(545, 24);
+            this.menuStrip1.TabIndex = 0;
+            // 
+            // equiposToolStripMenuItem
+            // 
+            this.equiposToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.equiposToolStripMenuItem1,
+            this.jugadoresToolStripMenuItem,
+            this.estadiosToolStripMenuItem});
+            this.equiposToolStripMenuItem.Name = "equiposToolStripMenuItem";
+            this.equiposToolStripMenuItem.Size = new System.Drawing.Size(69, 20);
+            this.equiposToolStripMenuItem.Text = "Equipos";
+            // 
+            // equiposToolStripMenuItem1
+            // 
+            this.equiposToolStripMenuItem1.Name = "equiposToolStripMenuItem1";
+            this.equiposToolStripMenuItem1.Size = new System.Drawing.Size(139, 22);
+            this.equiposToolStripMenuItem1.Text = "Equipos";
+            this.equiposToolStripMenuItem1.Click += new System.EventHandler(this.equiposToolStripMenuItem1_Click);
+            // 
+            // jugadoresToolStripMenuItem
+            // 
+            this.jugadoresToolStripMenuItem.Name = "jugadoresToolStripMenuItem";
+            this.jugadoresToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.jugadoresToolStripMenuItem.Text = "Jugadores";
+            this.jugadoresToolStripMenuItem.Click += new System.EventHandler(this.jugadoresToolStripMenuItem_Click);
+            // 
+            // estadiosToolStripMenuItem
+            // 
+            this.estadiosToolStripMenuItem.Name = "estadiosToolStripMenuItem";
+            this.estadiosToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.estadiosToolStripMenuItem.Text = "Estadios";
+            this.estadiosToolStripMenuItem.Click += new System.EventHandler(this.estadiosToolStripMenuItem_Click);
+            // 
+            // estadísticasToolStripMenuItem
+            // 
+            this.estadísticasToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.partidosToolStripMenuItem,
+            this.estadísticasDeEquiposToolStripMenuItem,
+            this.bateadoresToolStripMenuItem,
+            this.pitchersToolStripMenuItem,
+            this.clasificaciónToolStripMenuItem});
+            this.estadísticasToolStripMenuItem.Name = "estadísticasToolStripMenuItem";
+            this.estadísticasToolStripMenuItem.Size = new System.Drawing.Size(94, 20);
+            this.estadísticasToolStripMenuItem.Text = "Estadísticas";
+            // 
+            // partidosToolStripMenuItem
+            // 
+            this.partidosToolStripMenuItem.Name = "partidosToolStripMenuItem";
+            this.partidosToolStripMenuItem.Size = new System.Drawing.Size(223, 22);
+            this.partidosToolStripMenuItem.Text = "Partidos";
+            this.partidosToolStripMenuItem.Click += new System.EventHandler(this.partidosToolStripMenuItem_Click);
+            // 
+            // estadísticasDeEquiposToolStripMenuItem
+            // 
+            this.estadísticasDeEquiposToolStripMenuItem.Name = "estadísticasDeEquiposToolStripMenuItem";
+            this.estadísticasDeEquiposToolStripMenuItem.Size = new System.Drawing.Size(223, 22);
+            this.estadísticasDeEquiposToolStripMenuItem.Text = "Estadísticas de Equipos";
+            this.estadísticasDeEquiposToolStripMenuItem.Click += new System.EventHandler(this.estadísticasDeEquiposToolStripMenuItem_Click);
+            // 
+            // bateadoresToolStripMenuItem
+            // 
+            this.bateadoresToolStripMenuItem.Name = "bateadoresToolStripMenuItem";
+            this.bateadoresToolStripMenuItem.Size = new System.Drawing.Size(223, 22);
+            this.bateadoresToolStripMenuItem.Text = "Bateadores";
+            this.bateadoresToolStripMenuItem.Click += new System.EventHandler(this.bateadoresToolStripMenuItem_Click);
+            // 
+            // pitchersToolStripMenuItem
+            // 
+            this.pitchersToolStripMenuItem.Name = "pitchersToolStripMenuItem";
+            this.pitchersToolStripMenuItem.Size = new System.Drawing.Size(223, 22);
+            this.pitchersToolStripMenuItem.Text = "Pitchers";
+            this.pitchersToolStripMenuItem.Click += new System.EventHandler(this.pitchersToolStripMenuItem_Click);
+            // 
+            // clasificaciónToolStripMenuItem
+            // 
+            this.clasificaciónToolStripMenuItem.Name = "clasificaciónToolStripMenuItem";
+            this.clasificaciónToolStripMenuItem.Size = new System.Drawing.Size(223, 22);
+            this.clasificaciónToolStripMenuItem.Text = "Clasificación";
+            this.clasificaciónToolStripMenuItem.Click += new System.EventHandler(this.clasificaciónToolStripMenuItem_Click);
+            // 
+            // aficionadosToolStripMenuItem
+            // 
+            this.aficionadosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.horariosToolStripMenuItem,
+            this.plataformasDeDistribuciónToolStripMenuItem});
+            this.aficionadosToolStripMenuItem.Name = "aficionadosToolStripMenuItem";
+            this.aficionadosToolStripMenuItem.Size = new System.Drawing.Size(90, 20);
+            this.aficionadosToolStripMenuItem.Text = "Aficionados";
+            // 
+            // horariosToolStripMenuItem
+            // 
+            this.horariosToolStripMenuItem.Name = "horariosToolStripMenuItem";
+            this.horariosToolStripMenuItem.Size = new System.Drawing.Size(245, 22);
+            this.horariosToolStripMenuItem.Text = "Horarios";
+            this.horariosToolStripMenuItem.Click += new System.EventHandler(this.horariosToolStripMenuItem_Click);
+            // 
+            // plataformasDeDistribuciónToolStripMenuItem
+            // 
+            this.plataformasDeDistribuciónToolStripMenuItem.Name = "plataformasDeDistribuciónToolStripMenuItem";
+            this.plataformasDeDistribuciónToolStripMenuItem.Size = new System.Drawing.Size(245, 22);
+            this.plataformasDeDistribuciónToolStripMenuItem.Text = "Plataformas de distribución";
+            this.plataformasDeDistribuciónToolStripMenuItem.Click += new System.EventHandler(this.plataformasDeDistribuciónToolStripMenuItem_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 39);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Reporte Equipos";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(130, 39);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(153, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Reporte Clasificación";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(289, 39);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(208, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Reporte Estadísticas de Equipos";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MintCream;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(545, 306);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.Text = "Menú Principal";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem equiposToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem equiposToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem jugadoresToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem estadiosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem estadísticasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem partidosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aficionadosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem estadísticasDeEquiposToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem bateadoresToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pitchersToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clasificaciónToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem horariosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem plataformasDeDistribuciónToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,100 @@
+﻿using LMP.Formularios;
+using LMP.Informes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LMP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void equiposToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            frmEquipos x = new frmEquipos();
+            x.Show();
+        }
+
+        private void jugadoresToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmJugadores x = new frmJugadores();
+            x.Show();
+        }
+
+        private void estadiosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmEstadios x = new frmEstadios();
+            x.Show();
+        }
+
+        private void partidosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmPartidos x = new frmPartidos();
+            x.Show();
+        }
+
+        private void estadísticasDeEquiposToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmEstadisticasDeEquipos x = new frmEstadisticasDeEquipos();
+            x.Show();
+        }
+
+        private void bateadoresToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmBateadores x = new frmBateadores();
+            x.Show();
+        }
+
+        private void pitchersToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmPitchers x = new frmPitchers();
+            x.Show();
+        }
+
+        private void clasificaciónToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmClasificacion x = new frmClasificacion();
+            x.Show();
+        }
+
+        private void horariosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmHorarios x = new frmHorarios();
+            x.Show();
+        }
+
+        private void plataformasDeDistribuciónToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmPlataformasDeDistribucion x = new frmPlataformasDeDistribucion();
+            x.Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            frmREquipos x = new frmREquipos();
+            x.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            frmREClasificacion x = new frmREClasificacion();
+            x.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            frmRE_EstadisticasEquipos x = new frmRE_EstadisticasEquipos();
+            x.Show();
+        }
+    }
+}
